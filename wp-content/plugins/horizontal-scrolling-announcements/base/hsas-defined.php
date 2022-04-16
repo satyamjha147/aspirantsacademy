dd8e751c16119d53ceb88c43296a43e2088d6369
satyamjha147 <SATYAM.JHA1@svkmmumbai.onmicrosoft.com>
2022-04-16 09:28:20 +0530

    yes_commit

--- wp-content/plugins/horizontal-scrolling-announcements/base/hsas-defined.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; 
-}
-
-if(!defined('HSAS_FAVURL')) define('HSAS_FAVURL', 'http://www.gopiplus.com/work/2010/07/18/horizontal-scrolling-announcement/' );
